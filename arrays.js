1b35553fedbb8babf5f39ad49fbd40bf2ca8aa12
kafeero07 <51476794+kafeero07@users.noreply.github.com>
2019-06-21 09:16:19 +0300

    Update arrays.js

---
+++ arrays.js
@@ -0,0 +1,28 @@
+var primeNumber = (endNumber) =>{
+	var selectedPrimeNumbers =[];
+	var arrayOfNumbers = [ ...Array(endNumber).keys() ];//Generates sequencial numbers from 0 to 100 and stores in array arrayOfNumbers
+	//let i=0;
+	//while(i<endNumber)yield i++
+
+	for (var i=0;i<arrayOfNumbers.length;i++){ //loops through array
+		if(arrayOfNumbers[i]<2){
+			return false;
+			break;}
+    	 if((arrayOfNumbers[i]%2)===0){
+			    return false;
+			    break;}}
+			   
+		//else 
+		//return true;
+    	//for(var i=arrayOfNumbers[i]; i<arrayOfNumbers[arrayOfNumbers.length-1]; i++){
+			//selectedPrimeNumbers[]=i;
+			
+		selectedPrimeNumbers.push(arrayOfNumbers[i]); //pushes even numbers to the array called selectedPrimeNumbers
+			//console.log(arrayOfNumbers.length);
+		
+		console.log(selectedPrimeNumbers);
+	}
+		
+
+
+primeNumber(100);
